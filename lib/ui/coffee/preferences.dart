b2ae6eb4a6681afb2de18e24e5912e7d81a69783
yanna-torres <torres.yanna03@gmail.com>
2023-03-27 17:51:00 -0300

    initial commit: firebase studies :)

---
+++ lib/ui/coffee/preferences.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyPreferencesPage extends StatefulWidget {
+  const MyPreferencesPage({super.key});
+
+  @override
+  State<MyPreferencesPage> createState() => _MyPreferencesPageState();
+}
+
+class _MyPreferencesPageState extends State<MyPreferencesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
